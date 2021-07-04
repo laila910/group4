8735d90b3a9e836004e221701a2955a87712ff2b
MrWeber94 <abdelkareemelsayed2014@gmail.com>
2021-07-04 21:22:29 +0700

    v7.4

---
+++ index.php
@@ -0,0 +1,195 @@
+<?php 
+
+//   (//,#) Single line comment.
+//   (/**/) Multi line Comment.
+
+# code php 
+
+/*
+$age = 20;
+$age = 20;
+$age = 20;
+$age = 20;
+$age = 20;
+$age = 20;
+$age = 20;
+$age = 20;
+$age = 20;
+$age = 20;
+$age = 20;
+$age = 20;
+$age = 20;
+$age = 20;
+$age = 20;
+$age = 20;
+$age = 20;
+*/
+
+
+  //echo 'nti course (php)';
+  //  ECHO 'nti course';
+ // print('print function ');
+//echo ''; = echo "";
+
+
+
+//    define("age",20);   // constant var 
+
+//     // echo age;
+//     // echo constant('age');
+
+  
+//      echo '<h3> age 2 = '.age.'</h3><br>';
+
+
+//     $name = "ahmed";
+//     $age  = 30;
+
+//      echo "name".$name.'<br>';
+
+//      echo "age".$age;
+
+ //  echo '<h2>  age 3 ='.$age.'</h2>';
+
+
+
+   // + - * / % **
+
+
+   $num1 = 3;   // int
+   $num2 = "3"; // string
+
+//    echo ($num1+$num2);
+
+
+//    == ,  === , >,<, !=,<> , !== , <= , >=
+
+//  if($num1 === $num2){
+
+//     echo 'num1 = num2';
+// }
+
+
+
+//    $num3 = $num1;
+
+//    $num3 = $num3+2;
+
+    $num3 =2;
+
+//    echo $num3 *=5;
+
+
+// $num3 = $num3+1;
+
+// $num3 +=1;
+
+ //echo  $num3++;  //>>> 2
+
+// echo ++$num3;
+
+//  echo $num3;    //>>> 3 
+
+ // echo  --$num3;
+
+  //echo $num3;
+
+
+//     and , or , xor
+
+//    cond1 and (&&) con2    
+
+//    true      true  = true 
+//    true      false = false 
+//    false     true  = false 
+//    false     false = false 
+
+
+//    cond1  or (||) cond2 
+//    true      true  = true 
+//    true      false = true 
+//    false     true  = true 
+//    false     false = false 
+
+//    cond1  xor (xor) cond2
+//    true      true  =  false
+//    true      false =  true
+//    false     true  =  true
+//    false     false =  false
+
+//    !(true)  = false
+
+
+
+//   $name = "root";
+
+//   $name .= "test";
+
+//   $age = 20;
+
+//   echo $name.$age;
+
+
+    // ?: // ternary
+
+    // (condition)?actiontrue:actionFalse;
+
+//      $age = 20;
+
+//    $x =  ($age > 20)?'>20':'<= 20';
+// echo $x;
+
+
+// ['name','age']
+
+
+
+// $num = 7;
+
+// if ($num > 20) {
+//     # code...
+
+//     echo 'Number > 20';
+
+// }elseif( ($num < 20) &&  ($num > 10) ){
+
+//     echo 'Number between 10 and 20';
+// }else{
+
+//     echo 'Number < 10';
+    
+// }
+
+   
+$color = "x";
+
+  switch ($color) {
+      case 'green':
+          # code...
+          echo 'color is green';
+          break;
+
+      case 'blue':
+            # code...
+            echo 'color is blue';
+            break;  
+      
+       case 'red':
+                # code...
+                echo 'color is red';
+                break; 
+
+      default:
+          # code...
+          echo 'No Color Matched';
+          break;
+  }
+
+
+
+
+?>
+
+
+
+
