c201020e83ccd0b1abac5a940a75c784b143ab08
MrWeber94 <abdelkareemelsayed2014@gmail.com>
2021-07-26 15:48:17 +0700

    v7.26.2

--- Blog/Admin/db/gblog (2).sql
+++ Blog/Admin/db/gblog (2).sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jul 26, 2021 at 10:15 AM
+-- Generation Time: Jul 26, 2021 at 10:47 AM
 -- Server version: 10.4.14-MariaDB
 -- PHP Version: 7.4.9
 
@@ -62,6 +62,33 @@ INSERT INTO `admins` (`id`, `name`, `email`, `password`, `role_id`) VALUES
 (3, 'ahmed', 'soty@mailinator.com', 'ac748cb38ff28d1ea98458b16695739d7e90f22d', 8),
 (4, 'Seth Noble', 'vaqef@mailinator.com', 'ac748cb38ff28d1ea98458b16695739d7e90f22d', 8);
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `articalecategories`
+--
+
+CREATE TABLE `articalecategories` (
+  `id` int(11) NOT NULL,
+  `title` char(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `articales`
+--
+
+CREATE TABLE `articales` (
+  `id` int(11) NOT NULL,
+  `title` char(100) NOT NULL,
+  `content` text NOT NULL,
+  `image` char(50) NOT NULL,
+  `cat_id` int(11) NOT NULL,
+  `added_by` int(11) NOT NULL,
+  `date` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
 --
 -- Indexes for dumped tables
 --
@@ -80,6 +107,20 @@ ALTER TABLE `admins`
   ADD KEY `role_id` (`role_id`);
 
 --
+-- Indexes for table `articalecategories`
+--
+ALTER TABLE `articalecategories`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `articales`
+--
+ALTER TABLE `articales`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `cat_id` (`cat_id`),
+  ADD KEY `added_by` (`added_by`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -96,6 +137,18 @@ ALTER TABLE `admins`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
+-- AUTO_INCREMENT for table `articalecategories`
+--
+ALTER TABLE `articalecategories`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `articales`
+--
+ALTER TABLE `articales`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
 -- Constraints for dumped tables
 --
 
@@ -104,6 +157,13 @@ ALTER TABLE `admins`
 --
 ALTER TABLE `admins`
   ADD CONSTRAINT `relationRole` FOREIGN KEY (`role_id`) REFERENCES `adminroles` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `articales`
+--
+ALTER TABLE `articales`
+  ADD CONSTRAINT `addedBy` FOREIGN KEY (`added_by`) REFERENCES `admins` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `category` FOREIGN KEY (`cat_id`) REFERENCES `articalecategories` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
